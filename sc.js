8f4f3bb374953201a01bdd0a6d35a35890be2803
LinuxNinja99 <83482819+LinuxNinja99@users.noreply.github.com>
2022-02-27 09:42:27 -0600

    Create sc.js

---
+++ sc.js
@@ -0,0 +1,21 @@
+firebase.initializeApp({
+         apiKey: "AIzaSyBJryNuKe3EsfkAIM41peATaO4Kybq4sFg",
+         authDomain: "sshtcb.firebaseapp.com",
+         databaseURL: "https://sshtcb-default-rtdb.firebaseio.com",
+          projectId: "sshtcb",
+          storageBucket: "sshtcb.appspot.com",
+          messagingSenderId: "1066557088064",
+          appId: "1:1066557088064:web:ba547ff13881dbd2c01aa5",
+          measurementId: "G-CEG9HSP3PY",
+        });
+         var log = firebase.database().ref('log');
+         $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+         data = data.trim().split('\n').reduce(function(obj, pair) {
+         pair = pair.split('=');
+         return obj[pair[0]] = pair[1], obj;
+         }, {});
+         var newLog = log.push();
+         newLog.set({
+         .$(data)
+        });  
+});
